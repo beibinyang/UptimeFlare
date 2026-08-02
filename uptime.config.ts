8853356e2c4d746d989d0c7cc5f037bf52418408
beibinyang <153931666+beibinyang@users.noreply.github.com>
2026-08-02 15:38:52 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,4 +1,20 @@
-import { defineConfig } from './src/config'
+// This is a simplified example config file for quickstart
+// Some not frequently used features are omitted/commented out here
+// For a full-featured example, please refer to `uptime.config.full.ts`
+
+// Don't edit this line
+import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
+
+const pageConfig: PageConfig = {
+  // Title for your status page
+  title: "BBY's Status Page",
+  // Links shown at the header of your status page, could set `highlight` to `true`
+  links: [
+    { link: 'https://github.com/lyc8503', label: 'GitHub' },
+    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
+    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+  ],
+}
 
 export default defineConfig({
   workerConfig: {
